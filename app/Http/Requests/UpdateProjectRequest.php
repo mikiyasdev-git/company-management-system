7e0a2521a69bcd30377317192bc9aaacd38937bb
Mikiyas Mulugeta <mikiyas.connect@gmail.com>
2026-07-30 15:45:57 +0300

    Refactor prject store and update validation using form request

---
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:3',
+        'description' => 'nullable',
+        'start_date' => 'required|date',
+        'end_date' => 'nullable|date|after_or_equal:start_date',
+        'user_id' => 'required|exists:users,id',
+        'status' => 'required',
+        ];
+    }
+}
